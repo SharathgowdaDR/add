0e97b0ed927b0548d5603d55e5a3bd39cda5dac4
SharathgowdaDR <sharathgowdadr63>
2023-09-07 13:13:40 +0530

    code

---
+++ src/components/ExpensesItem.js
@@ -0,0 +1,11 @@
+function ExpensesItem(){
+    return (
+    <div>
+        <div>Expense Items</div>
+        <div>Food RS 10</div>
+        <div>Petrol RS 100</div>
+        <div>Movies Rs 200</div>
+    </div>
+    )
+}
+export default ExpensesItem;
